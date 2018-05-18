0ecb570b4b3b259dad58d9f48fcec6e084991023
LiorBy <38985079+LiorBy@users.noreply.github.com>
2018-05-18 04:40:56 -0700

    Add files via upload

---
+++ B18 Ex4 Lior 305346660 Gal 307880906/Ex04.Menus.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+
+
+        }
+
+    }
+}
