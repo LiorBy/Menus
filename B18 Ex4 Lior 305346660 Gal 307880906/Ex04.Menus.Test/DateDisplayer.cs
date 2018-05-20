75a190cbdfc9084ac7e5c795b409acc604d7f3bd
spykeMazor <klimazor@gmail.com>
2018-05-20 16:18:50 +0300

    16:18
    12345

---
+++ B18 Ex4 Lior 305346660 Gal 307880906/Ex04.Menus.Test/DateDisplayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex04.Menus.Interfaces;
+
+namespace Ex04.Menus.Test
+{
+    public class DateDisplayer : IAbilityToOperate
+    {
+        public void ExecutActionMethod()
+        {
+            showTodayDate();
+        }
+
+        private void showTodayDate()
+        {
+            Console.Clear();
+            char endDisplay;
+            Console.WriteLine("The Date Is: ");
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine("Press any key to end display...");
+            endDisplay = Console.ReadKey().KeyChar;
+            Console.Clear();
+        }
+    }
+}
---
+++ B18 Ex4 Lior 305346660 Gal 307880906/Ex04.Menus.Test/ShowVersion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex04.Menus.Interfaces;
+
+namespace Ex04.Menus.Test
+{
+    class ShowVersion
+    {
+        class CapitalLettersCounter : IAbilityToOperate
+        {
+            public void ExecutActionMethod()
+            {
+                showVersion();
+            }
+
+            private void showVersion()
+            {
+                char endDisplay;
+                Console.WriteLine("Version: 18.2.4.0");
+                Console.Clear();
+                Console.WriteLine("Press any key to end display...");
+                endDisplay = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
